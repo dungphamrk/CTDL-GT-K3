d2ae26ff732ecd3ec30f5d2f7a0320a6f4f9b0f8
Dung Pham <dungpham197qn@gmail.com>
2024-12-09 11:53:03 +0700

    fix

--- C/SS5/B10.c
+++ C/SS5/B10.c
@@ -1,54 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 struct Node
 {
     int data;
+    struct Node *prev;
     struct Node *next;
-    struct Node *pre;
 };
 typedef struct Node *node;
+
 node createNode(int data)
 {
-    node newNode = (node)malloc(sizeof(struct Node));
-    newNode->data = data;
-    newNode->next = NULL;
-    newNode->pre = NULL;
-    return newNode;
+    node temp = (node)malloc(sizeof(struct Node));
+    temp->data = data;
+    temp->prev = NULL;
+    temp->next = NULL;
+    return temp;
 }
-void convertToCircular(node *head)
-{
-    node temp = *head;
-    while (temp->next != NULL)
-    {
-        temp = temp->next;
-    }
-    node tail = temp;
-    temp = *head;
-    temp->pre = tail;
-    tail->next = temp;
-}
-void printList(node head)
-{
-    node currentNode = head;
-    while (currentNode != NULL)
-    {
-        printf("%d", currentNode->data);
-        if (currentNode->next != NULL)
-        {
-            printf(" <-> ");
-        }
-        currentNode = currentNode->next;
-    }
-    printf(" <-> NULL\n");
 
-    currentNode = head;
-    while (currentNode != NULL)
-    {
-        node temp = currentNode;
-        currentNode = currentNode->next;
-        free(temp);
-    }
-}
 void splitList(node head, node *evenHead, node *oddHead)
 {
     *evenHead = NULL;
@@ -68,7 +37,7 @@ void splitList(node head, node *evenHead, node *oddHead)
             else
             {
                 evenTail->next = newNode;
-                newNode->pre = evenTail;
+                newNode->prev = evenTail;
                 evenTail = newNode;
             }
         }
@@ -83,47 +52,77 @@ void splitList(node head, node *evenHead, node *oddHead)
             else
             {
                 oddTail->next = newNode;
-                newNode->pre = oddTail;
+                newNode->prev = oddTail;
                 oddTail = newNode;
             }
         }
         head = head->next;
     }
 }
+
+void printList(node head)
+{
+    while (head != NULL)
+    {
+        printf("%d <-> ", head->data);
+        head = head->next;
+    }
+    printf("NULL\n");
+}
+
+void freeList(node head)
+{
+    while (head != NULL)
+    {
+        node temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
 int main()
 {
     int n;
-    printf("Nhap so phan tu cua danh sach lien ket: ");
+    printf("Nhap so luong phan tu: ");
     scanf("%d", &n);
-    if (n <= 0)
-    {
-        printf("Danh sach trong");
-        return 0;
-    }
-    node head = NULL;
-    node tail = NULL;
-    node evenHead=NULL;
-    node oddHead=NULL;
+
+    node head = NULL, tail = NULL;
+
+    printf("Nhap cac phan tu cua danh sach lien ket doi:\n");
     for (int i = 0; i < n; i++)
     {
-        int data;
-        scanf("%d", &data);
-        node newNode = createNode(data);
+        int x;
+        scanf("%d", &x);
+        node temp = createNode(x);
         if (head == NULL)
         {
-            head = newNode;
-            tail = newNode;
+            head = temp;
+            tail = temp;
         }
         else
         {
-            tail->next = newNode;
-            newNode->pre = tail;
-            tail = newNode;
+            tail->next = temp;
+            temp->prev = tail;
+            tail = temp;
         }
     }
 
-    printf("Danh sach lien ket vua nhap: ");
-    splitList(&head,&evenHead,&oddHead);
+    printf("Danh sach ban dau: ");
     printList(head);
 
+    node evenHead, oddHead;
+    splitList(head, &evenHead, &oddHead);
+
+    printf("Danh sach cac phan tu chan: ");
+    printList(evenHead);
+
+    printf("Danh sach cac phan tu le: ");
+    printList(oddHead);
+
+    // Giải phóng bộ nhớ
+    freeList(head);
+    freeList(evenHead);
+    freeList(oddHead);
+
+    return 0;
 }
